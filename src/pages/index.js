e31df3d69a0b7cdfb122ce33543413cdfac48391
Manjilan <nakarmimanjila@yahoo.com>
2018-10-23 01:26:26 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Manjila's Gatsby Setup</h1>
+    <p>Web developer offering enthusiasm and understanding of various programming languages while learning new technologies and building on different skills. Would love to collaborate on new projects.</p>
+    <Link to="http://manjilanakarmi.com">Click here to checkout my Website!</Link>
+  </Layout>
+)
+
+export default IndexPage
